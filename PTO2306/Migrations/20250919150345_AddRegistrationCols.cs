cd5dc109e4efa5e99031b96ed695ccdc14ae0404
Dewald Breed <brddew8@protonmail.com>
2025-09-20 08:26:04 +0200

    Added missing migration

---
+++ PTO2306/Migrations/20250919150345_AddRegistrationCols.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PTO2306.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegistrationCols : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerifiedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerifiedAt",
+                table: "Users");
+        }
+    }
+}
