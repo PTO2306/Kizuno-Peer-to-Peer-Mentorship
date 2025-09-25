0b3531ba66a8333868b1f215b0ea1b0a192f2358
Dewald Breed <brddew8@protonmail.com>
2025-09-25 22:22:02 +0200

    Added basic theming foundation

--- UI/skillswap/src/main.tsx
+++ UI/skillswap/src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 import { StyledEngineProvider } from '@mui/material/styles';
 import GlobalStyles from '@mui/material/GlobalStyles';
+import { ThemeProvider } from '@mui/material';
+import { darkTheme, lightTheme } from './theme/theme';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <StyledEngineProvider enableCssLayer>
-      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
-      <App/>
-    </StyledEngineProvider>
-  </StrictMode>,
+  // <StrictMode>
+  <StyledEngineProvider enableCssLayer>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles styles='@layer theme, base, mui, components, utilities;' />
+      <App />
+    </ThemeProvider>
+  </StyledEngineProvider>
+  // </StrictMode>
 );
