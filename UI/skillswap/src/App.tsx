bd4e26de3f64957ea730fa83a472471d88efe2b4
Liezl Mouton <liz.mouton1@gmail.com>
2025-09-30 21:40:12 +0200

    Implement layout so navbar stays and only inner content changes

--- UI/skillswap/src/App.tsx
+++ UI/skillswap/src/App.tsx
@@ -1,16 +1,19 @@
-import { Routes, Route, BrowserRouter } from 'react-router';
-import { AuthProvider } from './auth/AuthContext';
-import AuthGuard from './auth/AuthGuard';
-import Notification from './components/Notification';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthContext";
+import AuthGuard from "./auth/AuthGuard";
+import Notification from "./components/Notification";
+
+// Layout
+import Layout from './components/Layout';
 
 // Pages
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import OnboardingPage from './pages/OnboardingPage';
-import DashboardPage from './pages/DashboardPage';
-import { ProfileProvider } from './auth/ProfileContext';
-import ProfilePage from './pages/ProfilePage';
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import DashboardPage from "./pages/DashboardPage";
+import { ProfileProvider } from "./auth/ProfileContext";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -22,22 +25,24 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/profile" element={<ProfilePage />}  />
-            
-            {/* Protected routes */}
+
+            {/* Protected routes with layout */}
+            <Route
+              element={
+                <AuthGuard requireProfile={true}>
+                  <Layout />
+                </AuthGuard>
+              }
+            >
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
+
             <Route
               path="/onboarding"
               element={
                 <AuthGuard>
                   <OnboardingPage />
-                </AuthGuard>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <AuthGuard requireProfile={true}>
-                  <DashboardPage />
                 </AuthGuard>
               }
             />
