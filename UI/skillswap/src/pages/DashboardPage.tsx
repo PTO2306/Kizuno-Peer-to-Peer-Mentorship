3f46bef5fd300aad39be95ca05884fdbc8ec0cd4
Liezl Mouton <liz.mouton1@gmail.com>
2025-10-07 18:17:54 +0200

    minor change

--- UI/skillswap/src/pages/DashboardPage.tsx
+++ UI/skillswap/src/pages/DashboardPage.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
-import Card from '../components/UI components/card/Card';
+import ClassCard from '../components/UI components/card/Card';
+
+const exampleData = [{
+    avatar: '',
+    title: 'Developer interview practice',
+    mentor: 'Coding Jesus',
+    subtitle: 'Brush up on your interview skills with an experienced SWE',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
+},
+{
+    avatar: '',
+    title: 'Insurance advice',
+    mentor: 'Chad Sharrock',
+    subtitle: 'Chat with an experienced and passionate insurance broker about your options',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
+},
+{
+    avatar: '',
+    title: 'ADHD Coaching',
+    mentor: 'Natalia Berghoff',
+    subtitle: 'Happy to have a chat about ADHD and daily challenges!',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
+}
+]
 
 const DashboardPage: React.FC = () => {
 
@@ -31,34 +54,16 @@ const DashboardPage: React.FC = () => {
             }}>
                 <h2>Quick Actions</h2>
                 <div className="flex flex-wrap gap-4 max-h-[80vh] overflow-y-auto p-4">
-                    <Card
-                        avatar={''}
-                        title='Developer interview practice'
-                        mentor='Coding Jesus'
-                        subtitle='Brush up on your interview skills with an experienced SWE'
-                        desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
-                    />
-                    <Card
-                        avatar={''}
-                        title='Insurance advice'
-                        mentor='Chad Sharrock'
-                        subtitle='Chat with an experienced and passionate insurance broker about your options'
-                        desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
-                    />
-                    <Card
-                        avatar={''}
-                        title='ADHD Coaching'
-                        mentor='Natalia Berghoff'
-                        subtitle='Happy to have a chat about ADHD and daily challenges!'
-                        desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
-                    />
-                    <Card
-                        avatar={''}
-                        title='Dota 2: Skill Reviews'
-                        mentor='Jenkins'
-                        subtitle='Let me help you evaluate your Dota skills and help you rank up'
-                        desc='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis ultrices felis, ac sodales purus cursus vel. Suspendisse vitae vestibulum odio. Integer sem dui, rutrum sit amet arcu vitae sed.'
-                    />
+                    {exampleData.map((item, index) => (
+                        <ClassCard
+                            key={`${item.title}-${index}`}
+                            avatar={item.avatar}
+                            title={item.title}
+                            mentor={item.mentor}
+                            subtitle={item.subtitle}
+                            desc={item.desc}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
